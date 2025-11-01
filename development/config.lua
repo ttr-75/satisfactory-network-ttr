cd3345d0b757a293a2c35d8564bd9f147b01fd53
Timo Nolte <timo@blum-nolte.de>
2025-11-01 22:02:28 +0100

    Update log level and configuration intervals in TTR_FIN_Config

--- development/config.lua
+++ development/config.lua
@@ -1,10 +1,10 @@
 local TTR_FIN_Config = {}
 
 TTR_FIN_Config.language = "de"
-TTR_FIN_Config.LOG_LEVEL = 0
+TTR_FIN_Config.LOG_LEVEL = 1
 
-TTR_FIN_Config.EVENT_LOOP_TIMEOUT = 0.5 -- in seconds
-TTR_FIN_Config.FACTORY_SCREEN_UPDATE_INTERVAL = 1 -- in seconds
+TTR_FIN_Config.EVENT_LOOP_TIMEOUT = 0.5           -- in seconds
+TTR_FIN_Config.FACTORY_SCREEN_UPDATE_INTERVAL = 2 -- in seconds
 
 
 _G.TTR_FIN_Config = TTR_FIN_Config
